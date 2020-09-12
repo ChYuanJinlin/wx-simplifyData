c942076c80e0868b8dc4e7705c4b19b8d366a8a8
袁金林 <1075360356@qq.com>
2020-09-12 11:51:37 +0800

    初始化第一次提交

---
+++ wx-demo/app.js
@@ -0,0 +1,11 @@
+// import simplify from './wx-simplifyData/index'
+import simplify from 'wx-simplify-data'
+import api from './request/request'
+simplify.init({
+  api
+})
+App({
+  onLaunch: function () {
+
+  }
+})
---
+++ wx-demo/examples/demo3/index.js
@@ -0,0 +1,38 @@
+const app = getApp()
+
+Page({
+  data: {
+    list: []
+  },
+  // 普通数据赋值
+  onLoad: function () {
+    wx.showLoading({
+      title: '获取数据中',
+    })
+    wx.$getData.call(this, wx.$am.getList1, {
+      // 你需要传给接口的数据
+      apiData: {},
+      // 配置statusCodeText字段
+      statusCodeText: 'code',
+      // 配置statusCode字段
+      statusCode: 666,
+      // 配置dataField字段
+      dataField: 'list',
+      toast:{
+        title:'这是自定义提示',
+        icon:'none'
+      },
+      // 需要赋值的字段
+      setData: 'list',
+      // 显示提示, 根据接口返回的msg提示
+      // showToast:true
+      // 自定义提示，根据微信的showToast写
+      // showToast:{
+      //   title:'数据获取成功!',
+      //   icon:'none'
+      // }
+
+    })
+
+  },
+})
