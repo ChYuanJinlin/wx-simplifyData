e9f68727be60b316c282919d11acab46a2cad090
yuanjinlin <1075360356@qq.com>
2021-08-05 17:12:45 +0800

    更新代码

--- wx-demo/request/request.js
+++ wx-demo/request/request.js
@@ -1,15 +1,7 @@
 // 模拟api 请求的数据
 const api = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> d69962e29e0a7f9b760a1320ccbccc1cc014b5d1
-=======
-
->>>>>>> a44c52728696ab5ff3c94cc2c09c01a1de96d27a
   getList() {
-    return new Promise((resovle, reject) => {
+    return new Promise((resolve, reject) => {
       const data = {
         msg: '成功',
         status: 200,
@@ -26,7 +18,7 @@ const api = {
       }
 
       setTimeout(() => {
-        resovle(data)
+        resolve(data)
       }, 1000);
 
 
@@ -34,21 +26,12 @@ const api = {
   },
 
   getList1() {
-    return new Promise((resovle, reject) => {
+    return new Promise((resolve, reject) => {
       const data = {
         msg: '成功',
         code: 666,
-<<<<<<< HEAD
-<<<<<<< HEAD
-        list: [
-          {
-=======
         list: [{
->>>>>>> d69962e29e0a7f9b760a1320ccbccc1cc014b5d1
-=======
-        list: [
-          {
->>>>>>> a44c52728696ab5ff3c94cc2c09c01a1de96d27a
+
             title: '这是模拟获取的数据'
           },
           {
@@ -61,7 +44,7 @@ const api = {
       }
 
       setTimeout(() => {
-        resovle(data)
+        resolve(data)
       }, 1000);
 
 
@@ -69,7 +52,7 @@ const api = {
   },
 
   getPageList(obj) {
-    return new Promise((resovle, reject) => {
+    return new Promise((resolve, reject) => {
       const data = {
         msg: '成功',
         status: 200,
@@ -79,20 +62,13 @@ const api = {
       setTimeout(() => {
 
         for (let index = 0; index < obj.page * 20; index++) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
           data.data.push({
-=======
-          data.list.push({
->>>>>>> d69962e29e0a7f9b760a1320ccbccc1cc014b5d1
-=======
-          data.data.push({
->>>>>>> a44c52728696ab5ff3c94cc2c09c01a1de96d27a
             title: '模拟分页数据'
           })
         }
 
-        resovle(data)
+        resolve(data)
       }, 1000);
 
 
